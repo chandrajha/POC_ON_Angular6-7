9579223d02042fc6c34335067104cfe2150c8f55
amresh <amresh.chandra@crestsolution.com>
2019-12-18 08:31:45 +0530

    code updated regarding folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BlogpostModule} from './blogpost/blogpost.module';
+import {CmspageModule} from './cmspage/cmspage.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BlogpostModule,
+    CmspageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
