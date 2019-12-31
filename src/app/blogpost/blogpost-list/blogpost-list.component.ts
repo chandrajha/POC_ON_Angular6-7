85fc60468b00c50c5d3b4672b1350305aeb91a91
amresh <amresh.chandra@crestsolution.com>
2019-12-31 07:42:24 +0530

    code updated for api configuration

--- src/app/blogpost/blogpost-list/blogpost-list.component.ts
+++ src/app/blogpost/blogpost-list/blogpost-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BlogpostService } from '../blogpost.service';
+import { Title } from '@angular/platform-browser';
+import { Blogpost } from '../blogpost';
 @Component({
   selector: 'app-blogpost-list',
   templateUrl: './blogpost-list.component.html',
@@ -8,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 export class BlogpostListComponent implements OnInit {
 
   title = 'Blogs';
-  constructor() { }
+  blogs: Blogpost;
+  error: {};
+  constructor(
+    private titleService: Title,
+    private blogpostService: BlogpostService
+  ) { }
 
   ngOnInit() {
+    this.titleService.setTitle(this.title);
+
+    this.blogpostService.getBlogs().subscribe(
+      (data: Blogpost) => this.blogs = data,
+      error => this.error = error
+    );
+
   }
 
 }
